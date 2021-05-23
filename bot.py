057a17c08c34087f0c8c8cc94e5a4a0ad82c1afc
wabalabudabdab <terekhovahork@gmail.com>
2021-05-23 19:06:12 +0300

    make new table with regs

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import sys
+from mysql.connector import cursor
 import telebot
 import mysql.connector
 from mysql.connector import errorcode
 
-bot = telebot.TeleBot("1889911207:AAE-3yWmjEmFRxA5OILdifDbZ-kTv9veuaM")
+# Telegram token
+bot = telebot.TeleBot("1787236564:AAGv_PwY13rM_1QN9aPJbKWWGpQx726AvMM")
+# получить id канала/группы
+# print(bot.get_chat('@vladneverovyoutube').id)
+
 
 try:
     db = mysql.connector.connect(
@@ -25,11 +30,16 @@ except mysql.connector.Error as err:
         sys.exit()
 
 
-
 cursor = db.cursor()
+# cursor.execute("DROP TABLE regs")
+# cursor.execute("CREATE TABLE regs (id INT AUTO_INCREMENT PRIMARY KEY, \
+#     first_name VARCHAR(255), \
+#     last_name VARCHAR(255), \
+#     description VARCHAR(255), user_id INT(11))")
+
 user_data = {}
 
-
+# cursor.execute("CREATE DATABASE dbbot")
 # cursor.execute("CREATE DATABASE watchman")
 # cursor.execute("SHOW DATABASES")
 
@@ -37,7 +47,7 @@ user_data = {}
 #    print(x)
 
 #cursor.execute("CREATE TABLE users (first_name VARCHAR(255), last_name VARCHAR(255))")
-#cursor.execute("SHOW TABLES")
+# cursor.execute("SHOW TABLES")
 
 #for x in cursor:
 #    print(x)
@@ -46,7 +56,6 @@ user_data = {}
 
 
 # sql = "INSERT INTO users (first_name, last_name, user_id) VALUES (%s, %s, %s)"
-# val = [("Серго", "Наумыч", 3), ("Матерь", "Драконов", 4), ("Добрый", "Друг", 5)]
 # cursor.executemany(sql, val)
 # db.commit()
 
@@ -63,26 +72,31 @@ user_data = {}
 
 # cursor.execute("ALTER TABLE users ADD COLUMN (user_group_id INT)")
 
-sql = "SELECT \
-    users.first_name AS user, \
-    user_groups.title AS user_group \
-    FROM users \
-    JOIN user_groups ON users.user_group_id = user_groups.id"
+# sql = "SELECT \
+#     users.first_name AS user, \
+#     user_groups.title AS user_group \
+#     FROM users \
+#     JOIN user_groups ON users.user_group_id = user_groups.id"
 
-cursor.execute(sql)
-users = cursor.fetchall()
+# cursor.execute(sql)
+# users = cursor.fetchall()
 
-for user in users:
-    print(user)
+# for user in users:
+#     print(user)
 
 
 class User:
     def __init__(self, first_name):
         self.first_name = first_name
         self.last_name = ''
+        self.description = ''
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
+        msg = bot.reply_to(message, """\
+        Привет, я бот-охранник в Istra Village.
+        """)
+
         msg = bot.send_message(message.chat.id, 'Введите имя')
         bot.register_next_step_handler(msg, process_firstname_step)
 
@@ -102,15 +116,42 @@ def process_lastname_step(message):
         user = user_data[user_id]
         user.last_name = message.text
 
-        sql = "INSERT INTO users (first_name, last_name, user_id) VALUES (%s, %s, %s)"
-        val = (user.first_name, user.last_name, user_id)
+        msg = bot.send_message(message.chat.id, 'Напишите описание')
+        bot.register_next_step_handler(msg, process_description_step)
+
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+def process_description_step(message):
+    try:
+        user_id = message.from_user.id
+        user = user_data[user_id]
+        user.description = message.text
+
+        # Проверка есть ли пользователь в БД
+        sql = "SELECT * FROM users WHERE user_id = {0}".format(user_id)
+        cursor.execute(sql)
+        existsUser = cursor.fetchone()
+
+        # Если нету, то добавить в БД
+        if (existsUser == None):
+               sql = "INSERT INTO users (first_name, last_name, telegram_user_id) \
+                                  VALUES (%s, %s, %s)"
+               val = (message.from_user.first_name, message.from_user.last_name, user_id)
+               cursor.execute(sql, val)
+
+        # Регистрация заявки
+        sql = "INSERT INTO regs (first_name, last_name, description, user_id) \
+                                  VALUES (%s, %s, %s, %s)"
+        val = (user.first_name, user.last_name, user.description, user_id)
         cursor.execute(sql, val)
         db.commit()
 
 
-        msg = bot.send_message(message.chat.id, 'Вы успешно зарегистрированы')
+        bot.send_message(message.chat.id, "Вы успешно зарегистрированны!")
+
     except Exception as e:
-        bot.reply_to(message, 'Вы уже зарегистрированы')
+        bot.reply_to(message, 'oooops')
 # make some difference
 
 # Enable saving next step handlers to file "./.handlers-saves/step.save".
@@ -123,4 +164,4 @@ bot.enable_save_next_step_handlers(delay=2)
 bot.load_next_step_handlers()
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.polling(none_stop = True)
